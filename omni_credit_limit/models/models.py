cc06bacbce31201540978c8b18f9c76930d43763
ckevangelista <kate@omnitechnical.com>
2020-04-09 06:56:34 +0000

    added logic for computation of receivables + uninvoiced but confirmed SO

--- omni_credit_limit/models/models.py
+++ omni_credit_limit/models/models.py
@@ -28,3 +28,48 @@ class SaleOrder(models.Model):
         ('done', 'Locked'),
         ('cancel', 'Cancelled'),
         ], string='Status', readonly=True, copy=False, index=True, tracking=4, default='draft')
+
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        for order in self:
+            order.check_limit()
+
+        return res
+
+    def check_limit(self):    
+        self.ensure_one()
+        partner = self.partner_id
+        moveline_obj = self.env['account.move.line']
+        movelines = moveline_obj.search(
+            [('partner_id', '=', partner.id),
+             ('account_id.user_type_id.name', 'in', ['Receivable', 'Payable']),
+             ('full_reconcile_id', '=', False)]
+        )
+        debit, credit, total_unpaid = 0.0, 0.0, 0.0
+        for line in movelines:
+            credit += line.debit
+            debit += line.credit
+        sales = self.env['sale.order'].search([
+            ('partner_id', '=', self.partner_id.id), 
+            ('invoice_status', 'not in', [('invoiced')]),
+            ('state','in',[('sale')])
+            #i need to include state=done here. but state=sale is escluded when i add done
+            ])
+        msg = 'start'
+        for sale in sales:
+            total_unpaid += sale.amount_total
+            msg = msg + '\nname='+sale.name+'::invoice_status='+sale.invoice_status+'::state='+sale.state+'::amount='+str(sale.amount_total)
+        raise UserError(msg + '\nend'
+            '\ncredit='+str(credit)+
+            '\ndebit='+str(debit)+
+            '\ntotal_unpaid='+str(total_unpaid)+
+            '\npartner.credit_limit='+str(partner.credit_limit)
+        )
+        if (credit - debit + total_unpaid) > partner.credit_limit:
+            if not partner.over_credit:
+                msg = 'Confirming Sale Order is not allowed. \nTotal Due Amount =' \
+                      '%s!\nPlease check Partner Accounts or Credit Limit.' % (credit - debit + total_unpaid)
+                raise UserError(_('Credit Over Limits !\n' + msg))
+            partner.write({'credit_limit': credit - debit + self.amount_total})
+
+        return self
